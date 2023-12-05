10ca0e97fb35ab86b774496bc4311717ab0bbf80
Farhat Sumaiya <Farhattsumaiya@gmail.com>
2023-12-05 17:13:31 -0500

    completed mymap

--- phase-2-enumerable.js
+++ phase-2-enumerable.js
@@ -5,9 +5,39 @@ Array.prototype.myEach = function (callback){
     }
 }
 
-Array.prototype.myMap = function (callback){
-    for(let i = 0; i < this.length; i++) {
-        this[i] = callback(this[i]);
-    }
-    return this;
+
+
+// Array.prototype.myMap = function (callback){
+//   let arr = []
+//     this.myEach(function(el) {
+//       arr.push(callback(el));
+// });
+//   return arr;
+// }
+// const num = [1,2,3]
+// console.log(num.myMap(function(el){
+//   return (el*3)
+// }
+
+// console.log(numbers)
+
+
+
+Array.prototype.myMap = function (callback) {
+  let result = []
+  this.myEach(function(ele) {
+      result.push(callback(ele));
+  });
+
+
+  return result;
 }
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.myMap(function(num) {
+return num * num;
+}));
+
+
