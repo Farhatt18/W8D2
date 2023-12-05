ada10339d04d8143dd76968247f40982a8394db4
Almoe099 <almoe9@LAPTOP-I8VP6L2K>
2023-12-05 15:41:37 -0500

    myMap phase 2

--- phase-1-arrays.js
+++ phase-1-arrays.js
@@ -10,7 +10,7 @@ Array.prototype.uniq = function() {
   return arr;
 };
 
-console.log(array.uniq());
+// console.log(array.uniq());
 
 
 Array.prototype.twoSum = function(){
@@ -28,4 +28,21 @@ Array.prototype.twoSum = function(){
 };
 
 // console.log([-1, 0, 2, -2, 1].twoSum());
+
+Array.prototype.transpose = function(){
+  let arr = [];
+  let length = this.length
+
+  for (let i = 0; i < this.length; i++){
+    arr.push([]);
+  }
+
+  for (let i = 0; i < this.length; i++){
+    for (let j = 0; j < length; j++){
+      arr[j].push(this[i][j]);
+    }
+  }
+  return arr;
+};
+console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose())
 
--- phase-2-enumerable.js
+++ phase-2-enumerable.js
@@ -1,1 +1,13 @@
 // Your code here
+Array.prototype.myEach = function (callback){
+    for(let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+}
+
+Array.prototype.myMap = function (callback){
+    for(let i = 0; i < this.length; i++) {
+        this[i] = callback(this[i]);
+    }
+    return this;
+}
